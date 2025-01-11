2f07c44462e1edd934d844297c01de3a450f04e0
wiyco <72733890+wiyco@users.noreply.github.com>
2025-01-12 00:07:05 +0900

    feat: safer logging

--- cmd/app.go
+++ cmd/app.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"log"
+	"log/slog"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/wiyco/go-api-template/pkg/api"
@@ -15,5 +16,19 @@ func InitApi() {
 
 	api.InitRoutes(app)
 
-	log.Fatal(app.Listen(":8000"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // default port
+	}
+
+	slog.Info("starting server", "port", port)
+
+	err := app.Listen(":8000")
+	if err != nil {
+		slog.Error("failed to start server",
+			"port", port,
+			"error", err,
+		)
+		os.Exit(1)
+	}
 }
